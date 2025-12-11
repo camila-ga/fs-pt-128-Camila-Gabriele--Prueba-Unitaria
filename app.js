b3a69a84873a1fb7ceb184ac1d9f89f7e979228c
Camila Gabriele <camilagabriele@outlook.com.ar>
2025-12-11 20:58:02 +0000

    se agregan cambios de moneda en funcion

--- app.js
+++ app.js
@@ -24,18 +24,22 @@ const fromEuroToDollar = function(valueInEuro) {
 }
 
 const fromDollarToYen = function(dollar){
-    let valueInYen = dollar * 146.26;
+    let valueInYen = dollar * (oneEuroIs.JPY / oneEuroIs.USD);
     return valueInYen;
 }
+// console.log(fromDollarToYen(1));
+
 // 1€ --------- 1.07 USD
 // 1€ --------- 156.5 YEN
 // entonces puedo hacer 156.5 / 1.07 : 146.26 YEN = 1USD
 
     
 const fromYenToPound = function(yen){
-    let valueInPound = yen * 0.00556;
+   let valueInPound = yen * (oneEuroIs.GBP / oneEuroIs.JPY);
     return valueInPound;
 }
+// console.log(fromYenToPound(1));
+
 //1€ ------- 0.87 POUND
 //1€ ------- 156.5 YEN 
 // entonces puedo hacer 0.87 / 156.5 : 0.00556 POUND = 1YEN
